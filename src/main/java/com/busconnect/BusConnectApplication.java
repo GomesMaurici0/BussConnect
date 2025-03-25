9b3c7655ebea078e457ec866e0b57edb08ef1ebb
Mauricio Da Silva Gomes <mauriciosilva.ghoms@gmail.com>
2025-03-24 23:50:08 -0300

    Init teste JPA

---
+++ src/main/java/com/busconnect/BusConnectApplication.java
@@ -0,0 +1,13 @@
+package com.busconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusConnectApplication.class, args);
+    }
+
+}
