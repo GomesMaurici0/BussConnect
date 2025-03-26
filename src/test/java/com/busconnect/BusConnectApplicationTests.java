15d35f1ee3f1273af469c080ebbff13d8ef35e62
Mauricio Da Silva Gomes <mauriciosilva.ghoms@gmail.com>
2025-03-26 14:25:04 -0300

    Creat Project and Creat new User

---
+++ src/test/java/com/busconnect/BusConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
